20b4ffe2b09835a696125159d7e2fce4b9b1421c
sumit-fusioncharts <sumit@fusioncharts.com>
2016-07-06 02:51:33 +0530

    Add files via upload

---
+++ chartlibrary.js
@@ -0,0 +1,13 @@
+(function(window){
+'use strict'
+	function defineChart(){
+		var fc ={};
+		fc.render = function(){
+			
+		};
+		return fc;	
+	}
+	if(typeof(fc)==='undefined'){
+		window.fc = defineChart();
+	}
+})(window);
