fa1c057ccd7f56bd4e80787873b7dbf5f3771a73
Sumit Pal <sumit@fusioncharts.com>
2016-08-18 11:08:22 +0530

    adding custom events

--- js/loadjson.js
+++ js/loadjson.js
@@ -6,7 +6,7 @@ window.onload=function(){
     if ( xmlhttp.readyState === 4 && xmlhttp.status === 200 ) {
       mychart = new ParseData(JSON.parse(xmlhttp.responseText));
       chart = new Engine(mychart);
-      chart.render("column");
+      chart.render("line");
     }
   };
   xmlhttp.open('GET','json/data.json',true);
